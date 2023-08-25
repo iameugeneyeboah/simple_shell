682b5b89b9dffa0734feb94ea6e3e44d0ec25129
Eugene Yeboah <iameugene533@gmail.com>
2023-08-24 20:39:38 -0700

    update on environment

--- myshell_environ.c
+++ myshell_environ.c
@@ -85,7 +85,7 @@ int populate_env_list(info_t *info)
     list_t *node = NULL;
     size_t i;
 
-    for (i = 0; environ[i];
+    for (i = 0; environ[i];)
 	add_node_end(&node, environ[i], 0);
 	info->env = node;
     return (0);
