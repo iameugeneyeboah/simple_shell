61ac17dc22da82dfa448300d15b3793e0aa44bbe
Ewura Abena <estherawuah44@gmail.com>
2023-08-26 14:31:28 -0700

    set info

--- getinfo.c
+++ getinfo.c
@@ -9,7 +9,10 @@
  */
 void clear_info(info_t *info)
 {
-	/* Implementation details */
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
 }
 
 /**
@@ -23,7 +26,28 @@ void clear_info(info_t *info)
  */
 void set_info(info_t *info, char **av)
 {
-	/* Implementation details */
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
 }
 
 /**
@@ -37,5 +61,25 @@ void set_info(info_t *info, char **av)
  */
 void free_info(info_t *info, int all)
 {
-	/* Implementation details */
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+	if (all)
+	{
+		if (!info->cmd_buf)
+			free(info->arg);
+		if (info->env)
+			free_list(&(info->env));
+		if (info->history)
+			free_list(&(info->history));
+		if (info->alias)
+			free_list(&(info->alias));
+		ffree(info->environ);
+			info->environ = NULL;
+		bfree((void **)info->cmd_buf);
+		if (info->readfd > 2)
+			close(info->readfd);
+		_putchar(BUF_FLUSH);
+	}
 }
+
