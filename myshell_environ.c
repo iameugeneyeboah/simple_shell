42c8b61876dc2a1a7e3ecbb6695a15466c24b8f0
Ewura Abena <estherawuah44@gmail.com>
2023-08-25 22:04:10 -0700

    environ check

--- myshell_environ.c
+++ myshell_environ.c
@@ -1,32 +1,32 @@
 #include "myshell.h"
 
 /**
- * myenv - prints the current environment
+ * myshell_myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
-int myenv(myshell_info_t *info)
+int myshell_myenv(myshell_info_t *info)
 {
     myshell_print_list_str(info->myshell_env);
     return (0);
 }
 
 /**
- * my_getenv - gets the value of an environ variable
+ * myshell_getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
  * @name: env var name
  *
  * Return: the value
  */
-char *my_getenv(myshell_info_t *info, const char *name)
+char *myshell_getenv(myshell_info_t *info, const char *name)
 {
     myshell_list_t *node = info->myshell_env;
     char *p;
 
     while (node)
     {
-	p = starts_with(node->str, name);
+	p = myshell_starts_with(node->str, name);
 	if (p && *p)
 	return (p);
 	node = node->next;
@@ -35,52 +35,52 @@ char *my_getenv(myshell_info_t *info, const char *name)
 }
 
 /**
- * mysetenv - Initialize a new environment variable,
+ * myshell_mysentenv - Initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * Return: Always 0
  */
-int mysetenv(myshell_info_t *info)
+int myshell_mysetenv(myshell_info_t *info)
 {
     if (info->myshell_argc != 3)
     {
-	eputs("Incorrect number of arguments\n");
+	myshell_eputs("Incorrect number of arguments\n");
 	return (1);
     }
-    if (_setenv(info, info->myshell_argv[1], info->myshell_argv[2]))
+    if (mysetenv(info, info->myshell_argv[1], info->myshell_argv[2]))
 	return (0);
     return (1);
 }
 
 /**
- * myunsetenv - Remove an environment variable
+ * myshell_myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * Return: Always 0
  */
-int myunsetenv(myshell_info_t *info)
+int myshell_myunsetenv(myshell_info_t *info)
 {
     int i;
 
     if (info->myshell_argc == 1)
     {
-	eputs("Too few arguments.\n");
+	myshell_eputs("Too few arguments.\n");
 	return (1);
     }
     for (i = 1; i <= info->myshell_argc; i++)
-	_unsetenv(info, info->myshell_argv[i]);
+	myshell_unsetenv(info, info->myshell_argv[i]);
 
     return (0);
 }
 
 /**
- * populate_env_list - populates env linked list
+ * myshell_populate_env_list - populates env linked list
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
-int populate_env_list(myshell_info_t *info)
+int myshell_populate_env_list(myshell_info_t *info)
 {
     myshell_list_t *node = NULL;
     size_t i;
